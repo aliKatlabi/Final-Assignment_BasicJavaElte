c4a0b4c2742907dbbf1cb35f21bb55562795961c
ali katlabi <43451842+aliKatlabi@users.noreply.github.com>
2018-11-28 23:51:07 +0100

    models/Shop.java

--- userInterface.java
+++ userInterface.java
@@ -1,14 +1,18 @@
 
 import java.util.Scanner;
 
-import models.Shop;
+import models.*;
+
+import beings.*;
+
+import java.lang.Exception;
 
 public class userInterface{
 	
 	
 	public static void main(String[] args){
 		
-		booleac noExit	=	true;
+		boolean noExit	=	true;
 		
 		Scanner input	=	new Scanner(System.in);
 		
@@ -16,11 +20,11 @@ public class userInterface{
 		
 		while(noExit){
 			
-			System.out.printf("%nChoose from..%n%n1.Register Prize tickets%n%n12.List all prize tickets%n3.List only raffled tickets%n4.Create a new Oompa Loompa song%n");
+			System.out.printf("%nChoose from..%n%n1.Register Prize tickets%n2.List all prize tickets%n3.List only raffled tickets%n4.Create a new Oompa Loompa song%n");
 			
-			System.out.printf("5.Register Beings%n16.Register Products%n7.Ruffle tickets%n8.Register sale%n9.List winners%n10.Exit%n");
+			System.out.printf("5.Register Beings%n6.Register Products%n7.Ruffle tickets%n8.Register sale%n9.List winners%n10.Exit%n");
 			
-			int n = sc.nextInt();
+			int n = input.nextInt();
 			
 			switch(n){
 				
@@ -28,9 +32,10 @@ public class userInterface{
 				
 						System.out.print("enter ticket Code : ");
 						
-						String putCode = sc.next();
+						String putCode = input.next();
 						
 						shop.registerPrizeTicket(putCode);
+						
 						break;
 				case 2:
 				
@@ -47,46 +52,49 @@ public class userInterface{
 				
 				case 5:
 						System.out.printf("1.kid%n2.oompaLoompa%n");
-						int ans = sc.nextInt();
+						int ans = input.nextInt();
 						if(ans==1){
 							
 							
 							System.out.print("Code : ");
 							
-							int code = sc.nextInt();
+							int code = input.nextInt();
 							
 							System.out.print("name : ");
 							
-							String name =sc.next();
+							String name =input.next();
 							
 							System.out.print("birth day : ");
 								
-							String birthDay =sc.next();
+							String birthDay =input.next();
 							
 							System.out.print("place of birth : ");
 							
-							String placeOfBirth=sc.next();
-							
+							String placeOfBirth=input.next();
+							try{
+								
 							shop.registerBeing(new Kid(code,name,birthDay,placeOfBirth));
+							
+							}catch(Exception ParseException){System.out.println("unparsable");}
 						}
 						if(ans==2){
 							
 							
 							System.out.print("Code : ");
 							
-							int code	= sc.next();
+							int code	= input.nextInt();
 							
 							System.out.print("name : ");
 							
-							String name	=	sc.next();
+							String name	=	input.next();
 							
 							System.out.print("height : ");
 							
-							int height	=	sc.nextInt();
+							int height	=	input.nextInt();
 							
 							System.out.print("favorate food : ");
 							
-							String fFood=	sc.next();
+							String fFood=	input.next();
 							
 							shop.registerBeing(new OompaLoompa(code,name,height,fFood));
 							
@@ -103,15 +111,15 @@ public class userInterface{
 				case 6:
 						System.out.print("barcode : ");
 						
-						long barcode	= sc.nextLong();
+						long barcode	= input.nextLong();
 						
 						System.out.print("serial Number : ");
 						
-						String SN	=	sc.next();
+						String SN	=	input.next();
 						
 						System.out.print("product decription : ");
 						
-						String decription	=	sc.next();
+						String decription	=	input.next();
 						
 						shop.registerProducts(new Product(decription,barcode,SN));
 				
@@ -119,9 +127,10 @@ public class userInterface{
 						
 						System.out.print("number of tickets to ruffle : ");
 						
-						int rn = sc.nextInt();
+						int rn = input.nextInt();
 						
 						if(shop.ruffleTickets(rn)){
+							
 							System.out.println("done ruffling !");
 						}else{
 							//todo
@@ -131,11 +140,11 @@ public class userInterface{
 				case 8:
 						System.out.print("User Code : ");
 								
-						int userCode =sc.nextInt();
+						int userCode =input.nextInt();
 							
 						System.out.print("product BarCode : ");
 							
-						long productBarCode placeOfBirth=sc.nextLong();
+						long productBarCode =input.nextLong();
 							
 						shop.makeSall(userCode,productBarCode);
 				

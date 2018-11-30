234a45f9fd7b7df0a57d587bfa428f4d182f01d1
ali katlabi <43451842+aliKatlabi@users.noreply.github.com>
2018-11-30 02:03:04 +0100

    add|equals

---
+++ models/GoldenTicket.java
@@ -0,0 +1,79 @@
+package models;
+
+import java.util.Date;
+
+import java.text.SimpleDateFormat;
+
+import java.text.ParseException;
+
+
+public class GoldenTicket{
+	
+	private String code;
+	
+	private Date raffled;
+	
+	public GoldenTicket(){}
+	
+	public GoldenTicket(Date raffled){
+		
+	
+		this.raffled	=		raffled;
+		
+		
+	}
+	
+	
+	public GoldenTicket(String code, Date raffled){
+		
+	
+		this.raffled	=		raffled;
+		
+		this.code		=		code;
+		
+	}
+	
+	public String getCode(){
+		
+		return this.code;
+		
+	}
+	
+	public Date getRaffled(){
+		
+		return this.raffled;
+		
+	}
+	
+	public void setCode(String code){
+			
+		this.code		=		code;
+		
+	}
+	
+	public void setRaffled(Date raffled){
+		
+		
+		this.raffled 	= 		raffled;
+		
+	}
+	
+	public boolean isRaffled(){
+		
+		return 	this!=null;
+		
+	}
+	@Override
+	
+	public String toString(){
+		
+		if(this!=null){
+				return this.code +":on:"+ (this.raffled).toString();
+		}else{
+				return "No prize ticket";
+		}
+	}
+	
+	
+	
+}
---
+++ models/Product.java
@@ -0,0 +1,82 @@
+package models;
+
+
+
+public class Product{
+	
+	private String description;
+	
+	private long barcode;
+	
+	private String serialNumber;
+	
+	private GoldenTicket prizeTicket;
+	
+	public Product(){}
+	
+	public Product(String desc,long bCode, String sNum ){
+		
+		this.description	=	desc;
+		
+		this.barcode		=	bCode;
+		
+		this.serialNumber	=	sNum;
+		
+		this.prizeTicket	=	null;
+		
+	}
+	
+	public String getDescription(){
+		
+		return this.description;
+	}
+	public long getBarCode(){
+		
+		return this.barcode;
+	}
+	public String getSerialNumber(){
+		
+		return this.serialNumber;
+	}
+	public GoldenTicket getPrizeTicket(){
+		
+		return this.prizeTicket;
+	}
+	
+	public void setDescription(String desc){
+		
+		this.description	=	desc;
+	}
+	public void setBarCode(long bCode){
+		
+		this.barcode	=	bCode;
+		
+		
+	}
+	public void setSerialNumber(String sNum){
+		
+		this.serialNumber	=	sNum;
+	}
+	public void setPrizeTicket(GoldenTicket GoldenT){
+		
+		this.prizeTicket	=	GoldenT;
+		
+	}
+	
+	public boolean isGolden(){
+		
+		return (	this.prizeTicket	!=	null	)?true:false;
+	}
+	
+	@Override
+	
+	public String toString(){
+		
+		return 	 this.barcode +":" 
+				+this.serialNumber+":" 
+				+ this.description +":Prize Ticket:"
+				+ prizeTicket;
+	}
+	
+	
+}
